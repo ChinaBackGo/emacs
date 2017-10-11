f2b035771e8e500a72d0274664395714654e5d9d
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2017-10-11 15:17:11 +0800

    Added defaults from: https://github.com/technomancy/better-defaults.git

--- init.el
+++ init.el
@@ -1,19 +1,4 @@
 ;; -*- emacs-lisp -*-
-(package-initialize)                ;; Initialize & Install Package
+(add-to-list 'load-path "./better-defaults")
+(require 'better-defaults)
 ;; (setq org-...)                   ;; Your custom settings
-;; -*- emacs-lisp -*-
-(unless package-archive-contents    ;; Refresh the packages descriptions
-  (package-refresh-contents))
-(setq package-load-list '(all))     ;; List of packages to load
-(unless (package-installed-p 'org)  ;; Make sure the Org package is
-  (package-install 'org))           ;; installed, install it if not
-(package-initialize)                ;; Initialize & Install Package
-;; (setq org-...)                   ;; Your custom settings
-(require 'package)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/"))
-(package-initialize)
-
