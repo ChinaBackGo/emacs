09d450277f1e007b464f3cfce502baaafce43e39
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2017-10-11 16:09:44 +0800

    Adding melpa repo Added instructions to add markdown mode

--- init.el
+++ init.el
@@ -1,4 +1,25 @@
 ;; -*- emacs-lisp -*-
+;; install markdown mode
+;; M-x package-install RET markdown-mode RET after adding to init.el
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+;; add better-defaults
 (add-to-list 'load-path "./better-defaults")
 (require 'better-defaults)
+
+(package-initialize)
 ;; end of init.el
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (markdown-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
