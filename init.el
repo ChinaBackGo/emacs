de3fbbe4b88f8ba280c556cab152958030895856
ChinaBackGo <ChinaBackGo@users.noreply.github.com>
2017-10-12 11:30:24 +0800

    Removed Splash screen/scrollbar/toolbar/menu Moved all backup files to own backup directory

--- init.el
+++ init.el
@@ -1,4 +1,21 @@
 ;; -*- emacs-lisp -*-
+
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; No splash screen please ... jeez
+(setq inhibit-startup-message t)
+
+;; Write backup files to own directory
+(setq backup-directory-alist
+      `(("." . ,(expand-file-name
+                 (concat user-emacs-directory "backups")))))
+
+;; Make backups of files, even when they're in version control
+(setq vc-make-backup-files t)
+
 ;; install markdown mode
 ;; M-x package-install RET markdown-mode RET after adding to init.el
 (require 'package)
@@ -6,7 +23,8 @@
              '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 ;; add better-defaults
-(add-to-list 'load-path "./better-defaults")
+;; https://github.com/technomancy/better-defaults
+(add-to-list 'load-path (concat user-emacs-directory "better-defaults"))
 (require 'better-defaults)
 
 (package-initialize)
